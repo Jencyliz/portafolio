9bafce95bbd809d219479fe6663862db3ff8817c
liz <liz.yanez.lobe@gmail.com>
2024-10-24 23:39:45 -0300

    sobremi

--- dportafolio/src/components/AboutMe.vue
+++ dportafolio/src/components/AboutMe.vue
@@ -1,7 +1,11 @@
 <template>
-    <section id="about" class="my-5">
-      <h2>Sobre mí</h2>
-      <img src="../assets/icono1.svg" alt="imagenicono" padding="10px">
+<div class="container col-md-1" id="about">
+    <div class="row">
+  <div class="image col-md-6">
+    <img src="../assets/icono1.svg" alt="imagenicono" padding="10px">
+    </div>
+    <div class="text col-md-6">
+        <h2>Sobre mí</h2>
       <p>¡Hola! Soy Jency Yañez vivo en Chillan 
 
 Soy desarrolladora front-end y recientemente me he graduado de la carrera de [nombre de la carrera]. Durante mi formación, adquirí conocimientos en tecnologías como HTML, CSS y JavaScript, así como en frameworks como Vue y Bootstrap.
@@ -11,6 +15,27 @@ Me apasiona el desarrollo web porque me permite combinar mi creatividad con la l
 Además, tengo muchas ganas de aprender y seguir creciendo en este campo. Estoy abierta a nuevos desafíos y colaboraciones que me ayuden a ampliar mis habilidades y conocimientos.
 
 ¡Gracias por su atención! Estoy ansiosa por conectarme y explorar oportunidades en el mundo del desarrollo web.</p>
-    </section>
+    </div>
+  </div>
+</div>
+
   </template>
-  
+  <style>
+  .container {
+            width: 100%;
+        }
+
+        .image{
+            float: left;
+            width: 100%;
+            margin-top: 100px;
+        }
+
+        img {
+            width: 100%;
+            height: auto;
+        }
+        .text{
+            align-self: flex-end;
+        }
+</style>
