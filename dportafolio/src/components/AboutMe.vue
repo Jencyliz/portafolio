624dd569339360df298880a03c487009e4ca8add
liz <liz.yanez.lobe@gmail.com>
2024-10-25 23:38:20 -0300

    cambiosaboutme

--- dportafolio/src/components/AboutMe.vue
+++ dportafolio/src/components/AboutMe.vue
@@ -1,41 +1,57 @@
 <template>
-<div class="container col-md-1" id="about">
+  <div class="container" id="about">
     <div class="row">
-  <div class="image col-md-6">
-    <img src="../assets/icono1.svg" alt="imagenicono" padding="10px">
+      <div class="col-lg-6 my-lg-0 my-6">
+      <div class="image">
+        <p>
+        <img src="../assets/icono1.svg" alt="imagenicono" padding="10px" />
+      </p>
+      </div>
     </div>
-    <div class="text col-md-6">
+    <div class="col-lg-6 my-lg-0 my-6">
+      <div class="text">
         <h2>Sobre mí</h2>
-      <p>¡Hola! Soy Jency Yañez vivo en Chillan 
-
-Soy desarrolladora front-end y recientemente me he graduado de la carrera de [nombre de la carrera]. Durante mi formación, adquirí conocimientos en tecnologías como HTML, CSS y JavaScript, así como en frameworks como Vue y Bootstrap.
-
-Me apasiona el desarrollo web porque me permite combinar mi creatividad con la lógica técnica para crear experiencias interactivas y atractivas. Estoy emocionada por comenzar mi carrera profesional y aplicar lo que he aprendido en proyectos reales.
-
-Además, tengo muchas ganas de aprender y seguir creciendo en este campo. Estoy abierta a nuevos desafíos y colaboraciones que me ayuden a ampliar mis habilidades y conocimientos.
-
-¡Gracias por su atención! Estoy ansiosa por conectarme y explorar oportunidades en el mundo del desarrollo web.</p>
+        <p>
+          ¡Hola! Soy Jency Yañez vivo en Chillan Soy desarrolladora front-end y
+          recientemente me he graduado de la carrera de [nombre de la carrera].
+          Durante mi formación, adquirí conocimientos en tecnologías como HTML,
+          CSS y JavaScript, así como en frameworks como Vue y Bootstrap. Me
+          apasiona el desarrollo web porque me permite combinar mi creatividad
+          con la lógica técnica para crear experiencias interactivas y
+          atractivas. Estoy emocionada por comenzar mi carrera profesional y
+          aplicar lo que he aprendido en proyectos reales. Además, tengo muchas
+          ganas de aprender y seguir creciendo en este campo. Estoy abierta a
+          nuevos desafíos y colaboraciones que me ayuden a ampliar mis
+          habilidades y conocimientos. ¡Gracias por su atención! Estoy ansiosa
+          por conectarme y explorar oportunidades en el mundo del desarrollo
+          web.
+        </p>
+      </div>
+      </div>
     </div>
   </div>
-</div>
+</template>
+<style scoped>
+.container {
+  width: 100%;
+}
 
-  </template>
-  <style>
-  .container {
-            width: 100%;
-        }
+.image {
+  float: left;
+  width: 500px;
+  height: 500px;
+  margin-top: 100px;
+}
 
-        .image{
-            float: left;
-            width: 100%;
-            margin-top: 100px;
-        }
-
-        img {
-            width: 100%;
-            height: auto;
-        }
-        .text{
-            align-self: flex-end;
-        }
+img {
+  width: 100%;
+  height: auto;
+}
+.text {
+  float:right;
+  width: 500px;
+  height: 500px;
+  margin-top: 100px;
+  inline-size: 500px;
+}
 </style>
